25dc60354e384d1e094c6cc9015e81a7121d61a2
Nicolas Vera <nive7792@gmail.com>
2023-12-18 01:52:14 -0300

    refactor: Divide into components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { StatsComponent } from './stats/stats.component';
+import { FooterComponent } from './footer/footer.component';
+import { DevInfoComponent } from './dev-info/dev-info.component';
+import { MapComponent } from './map/map.component';
+import { SearcherComponent } from './searcher/searcher.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    StatsComponent,
+    FooterComponent,
+    DevInfoComponent,
+    MapComponent,
+    SearcherComponent
   ],
   imports: [
     BrowserModule,
